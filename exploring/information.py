192f0bef1c5bf29a57189fb6bf3f096071b06f62
Albert Florinus <abbeflorinus@gmail.com>
2022-02-22 13:50:59 +0100

    _

--- exploring/information.py
+++ exploring/information.py
@@ -1,4 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv("house-data/train.csv")
-print(df)
+#Dropping alley col, too much missing values
+df = df.drop(columns=["Alley"])
+
+print(df.dtypes)
+#print(df.keys())
--- test.py
+++
@@ -1,7 +0,0 @@
-print("hej")
-
-x = 3
-print(x+2)
-
-print('test')
-#Abbe
